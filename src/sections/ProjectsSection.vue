0da4ecd44cea2d1ccc45ec8f446689af61f87621
demaxl <aweabdullahi@gmail.com>
2025-02-05 18:30:39 +0100

    Remove csp from projects

--- src/sections/ProjectsSection.vue
+++ src/sections/ProjectsSection.vue
@@ -27,13 +27,13 @@ const projects = [
         description: `I developed a custom web application for BringoExpress, a logistics and shipment company, to optimize its operations. The application enables seamless tracking of shipments, manages payments and inventory, automates SMS alerts, and facilitates invoice generation. This solution was tailored to meet the company’s needs, ensuring efficiency and a better customer experience.`,
         imagePath: '/images/projects/bringo.png',
         projectLink: 'https://www.bringoexpress.com/'
-    },
-    {
-        title: 'College Soccer Pathway - College Soccer Recruitment App',
-        description: `I developed the backend API for the College Soccer Pathway mobile application, a platform that empowers high school and elite soccer players to proactively manage their college recruitment journey. 
-            The API supports features such as searching and filtering collegiate soccer programs nationwide, providing comprehensive information on coaches' contact details, team performance records, player rosters, and academic data. This robust backend ensures seamless data management and user interactions, facilitating athletes in making informed decisions and connecting directly with their desired college programs.`,
-        imagePath: '/images/projects/csp.webp',
-        projectLink: 'https://www.collegesoccerpathway.com/'
     }
+    // {
+    //     title: 'College Soccer Pathway - College Soccer Recruitment App',
+    //     description: `I developed the backend API for the College Soccer Pathway mobile application, a platform that empowers high school and elite soccer players to proactively manage their college recruitment journey.
+    //         The API supports features such as searching and filtering collegiate soccer programs nationwide, providing comprehensive information on coaches' contact details, team performance records, player rosters, and academic data. This robust backend ensures seamless data management and user interactions, facilitating athletes in making informed decisions and connecting directly with their desired college programs.`,
+    //     imagePath: '/images/projects/csp.webp',
+    //     projectLink: 'https://www.collegesoccerpathway.com/'
+    // }
 ]
 </script>
